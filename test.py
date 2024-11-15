b33ddb1ab8387a9cb6d207560cde7ad7991aec8e
Abdullah <abdullahrehan2010@gmail.com>
2024-11-15 14:56:55 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+a=6
+b="abdullah"
+c=6.98
+d=True
+e=str(d)
+print("boolean got converted", type(e))
+print(type(a))
+g=float(a)
+print ("conversion result", type(g))
+print(type(b))
+print(type(c))
+print(type(d))
